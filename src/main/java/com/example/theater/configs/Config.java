17332c529f64262f9d785a8eacc28117ad750e3f
sigitabra <sigita.zutautaite@gmail.com>
2024-06-03 21:44:27 +0300

    Enable validation error messages in LT

--- src/main/java/com/example/theater/configs/Config.java
+++ src/main/java/com/example/theater/configs/Config.java
@@ -2,18 +2,7 @@ package com.example.theater.configs;
 
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -21,45 +10,8 @@ import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 
 import java.util.Locale;
 
-@Configuration
-@EnableMethodSecurity
+
 public class Config implements WebMvcConfigurer {
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.csrf(AbstractHttpConfigurer::disable)
-                   .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
-                           .requestMatchers("/reservations/**").permitAll()
-                           .requestMatchers("**").permitAll()
-                           .anyRequest().authenticated()
-                   )
-                   .httpBasic(Customizer.withDefaults())
-                   .build();
-    }
-
-    @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails admin = User
-                .withUsername("admin")
-                .password(passwordEncoder().encode("admin123"))
-                .roles("ADMIN")
-                .build();
-
-        UserDetails coordinator = User
-                .withUsername("sigita")
-                .password(passwordEncoder().encode("sigita123"))
-                .roles("COORDINATOR")
-                .build();
-
-
-        return new InMemoryUserDetailsManager(admin, coordinator);
-    }
-
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(8);
-    }
-
     @Bean
     public LocaleResolver localeResolver() {
         CookieLocaleResolver cookieLocaleResolver = new CookieLocaleResolver();
@@ -84,5 +36,6 @@ public class Config implements WebMvcConfigurer {
         bean.setValidationMessageSource(messageSource());
         return bean;
     }
-
 }
+
+
